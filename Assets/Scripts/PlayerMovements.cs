a7290735e30f1521361edfb71b371999bb62c61f
Fuloo <51947133+IAmJustADuck@users.noreply.github.com>
2024-10-21 13:23:01 +0200

    Ajout ragdoll

--- Assets/Scripts/PlayerMovements.cs
+++ Assets/Scripts/PlayerMovements.cs
@@ -2,70 +2,92 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class PlayerMovements : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 5f;
-    [SerializeField] private float mouseSensitivity = 2f;
-    [SerializeField] private float jumpForce = 5f;
-    [SerializeField] private float minVerticalAngle = -90f;
-    [SerializeField] private float maxVerticalAngle = 90f;
-    [SerializeField] private float raycastDistance = 1.1f;
-    [SerializeField] private LayerMask groundMask;
+    [SerializeField] private Camera playerCamera;
+    [SerializeField] private float walkSpeed = 6f;
+    [SerializeField] private float runSpeed = 12f;
+    [SerializeField] private float jumpPower = 7f;
+    [SerializeField] private float gravity = 10f;
 
-    private float verticalRotation = 0f;
-    private Rigidbody rb;
-    private bool isGrounded;
+
+    [SerializeField] private float lookSpeed = 2f;
+    [SerializeField] private float lookXLimit = 45f;
+
+    [SerializeField] private LayerMask peopleRaycastMask;
+    [SerializeField] private GameObject playerShoulder;
+
+    private Vector3 moveDirection = Vector3.zero;
+    private float rotationX = 0;
+
+    [SerializeField] private bool canMove = true;
+
+    private CharacterController characterController;
 
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        characterController = GetComponent<CharacterController>();
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
 
     void Update()
     {
-        HandleLook();
-        HandleMovement();
+        #region Handles Movement
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 right = transform.TransformDirection(Vector3.right);
 
-        CheckGrounded();
+        bool isRunning = Input.GetKey(KeyCode.LeftShift);
+        float curSpeedX = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Vertical") : 0;
+        float curSpeedY = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Horizontal") : 0;
+        float movementDirectionY = moveDirection.y;
+        moveDirection = (forward * curSpeedX) + (right * curSpeedY);
 
-        if (Input.GetButtonDown("Jump") && isGrounded)
+        #endregion
+
+        #region Handles Jumping
+        if (Input.GetButton("Jump") && canMove && characterController.isGrounded)
         {
-            Jump();
+            moveDirection.y = jumpPower;
         }
-    }
+        else
+        {
+            moveDirection.y = movementDirectionY;
+        }
 
-    private void HandleLook()
-    {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+        if (!characterController.isGrounded)
+        {
+            moveDirection.y -= gravity * Time.deltaTime;
+        }
 
-        verticalRotation -= mouseY;
-        verticalRotation = Mathf.Clamp(verticalRotation, minVerticalAngle, maxVerticalAngle);
+        #endregion
 
-        transform.Rotate(0, mouseX, 0);
-        Camera.main.transform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
-    }
+        #region Handles Rotation
+        characterController.Move(moveDirection * Time.deltaTime);
 
-    private void HandleMovement()
-    {
-        float moveX = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
-        float moveZ = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        if (canMove)
+        {
+            rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
+            rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
+            playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+            transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
+        }
+        #endregion
 
-        Vector3 move = transform.right * moveX + transform.forward * moveZ;
-        transform.position += move;
-    }
-
-    private void Jump()
-    {
-        rb.AddForce(Vector3.up * jumpForce, ForceMode.VelocityChange);
-    }
-
-    private void CheckGrounded()
-    {
-        RaycastHit hit;
-        Vector3 rayOrigin = transform.position + Vector3.up * 0.1f;
-        isGrounded = Physics.Raycast(rayOrigin, Vector3.down, out hit, raycastDistance, groundMask);
+        #region Detect Object On click
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = playerCamera.ViewportPointToRay(new Vector3(.5f, .5f));
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100.0f, peopleRaycastMask))
+            {
+                Debug.Log("You selected the " + hit.transform.name + " | And parent = " + hit.transform.root);
+                hit.transform.root.transform.GetChild(0).GetComponent<PeopleScript>().EnableRagdolls();
+                hit.transform.root.transform.position = playerShoulder.transform.position;
+                hit.transform.root.transform.rotation = playerShoulder.transform.rotation;
+            }
+        }
+        #endregion
     }
 }
